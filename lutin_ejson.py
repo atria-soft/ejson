7c6e6a56b83cec2f526633cb5057ffb1711ab05a
Edouard DUPIN <yui.heero@gmail.com>
2013-12-06 21:46:38 +0100

    [DEV] Add http folder

--- lutin_ejson.py
+++ lutin_ejson.py
@@ -6,6 +6,9 @@ def Create(target):
 	myModule = lutinModule.module(__file__, 'ejson', 'LIBRARY')
 	# enable doculentation :
 	myModule.doc_enable()
+	myModule.documentation.set_website("http://HeeroYui.github.io/ejson/doc/doc/")
+	myModule.documentation.set_path(lutinTools.GetCurrentPath(__file__) + "/ejson/")
+	myModule.documentation.set_external_link(['ewol'])
 	
 	myModule.AddModuleDepend(['etk'])
 	
