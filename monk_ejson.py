fbf39d9ac20b7666f907154b58c8f7872bb1e271
Edouard DUPIN <yui.heero@gmail.com>
2013-12-28 16:09:29 +0100

    [DOC] add monk website

--- monk_ejson.py
+++ monk_ejson.py
@@ -10,6 +10,7 @@ def create():
 	myModule = module.Module(__file__, 'ejson', 'LIBRARY')
 	# enable doculentation :
 	myModule.set_website("http://heeroyui.github.io/ejson/")
+	myModule.set_website_sources("http://github.com/heeroyui/ejson/")
 	myModule.set_path(tools.get_current_path(__file__) + "/ejson/")
 	# add the currrent module at the 
 	return myModule
