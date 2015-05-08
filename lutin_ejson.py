f31f07bf7e8617ad80d5aef147536fa76b9d255c
Edouard DUPIN <yui.heero@gmail.com>
2015-05-08 22:37:25 +0200

    [BUILD] update new lutin 0.5.3

--- lutin_ejson.py
+++ lutin_ejson.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
-import lutinModule as module
-import lutinTools as tools
+import lutin.module as module
+import lutin.tools as tools
 
 def get_desc():
 	return "e-json Json parser and generator"
--- lutin_ejson_test.py
+++ lutin_ejson_test.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
-import lutinModule as module
-import lutinTools as tools
+import lutin.module as module
+import lutin.tools as tools
 import datetime
 
 def get_desc():
